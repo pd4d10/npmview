8b6f2c33d1f6147b537fe2edff2b37715dda44b5
Rongjian Zhang <pd4d10@gmail.com>
2019-07-04 22:07:16 +0800

    feat: add tree view

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Tree, ITreeNode } from '@blueprintjs/core'
 import './App.css'
 
+interface PackageMetaFile {
+  path: string
+  type: 'file'
+  contentType: 'text/markdown' | 'application/javascript'
+  integrity: string
+  lastModified: string
+  size: number
+}
+
+interface PackageMetaDirectory {
+  path: string
+  type: 'directory'
+  files: (PackageMetaFile | PackageMetaDirectory)[]
+}
+
+function convertMetaToTreeNode(
+  file: PackageMetaFile | PackageMetaDirectory,
+): ITreeNode {
+  switch (file.type) {
+    case 'directory':
+      return {
+        id: file.path,
+        icon: 'folder-close',
+        label: file.path,
+        childNodes: file.files.map(convertMetaToTreeNode),
+        isExpanded: true,
+      }
+    case 'file':
+      return {
+        id: file.path,
+        icon: 'document',
+        label: file.path,
+      }
+  }
+}
+
 const App: React.FC = () => {
-  return <div>app</div>
+  const [data, setData] = useState<ITreeNode>()
+
+  useEffect(() => {
+    ;(async () => {
+      const res = await fetch('https://unpkg.com/express@4.17.1/?meta')
+      const json = await res.json()
+      const node = convertMetaToTreeNode(json as PackageMetaDirectory)
+      setData(node)
+    })()
+  }, [])
+
+  return (
+    <div>{data && data.childNodes && <Tree contents={data.childNodes} />}</div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import 'normalize.css/normalize.css'
+import '@blueprintjs/core/lib/css/blueprint.css'
+import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
