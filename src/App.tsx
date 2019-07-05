566cd1564f54835aa5a10517aaba9ad02870876a
Rongjian Zhang <pd4d10@gmail.com>
2019-07-05 17:05:31 +0800

    feat: show files with highlight

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import path from 'path'
 import { Tree, ITreeNode } from '@blueprintjs/core'
+import numeral from 'numeral'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import './App.css'
 
 interface PackageMetaFile {
@@ -17,41 +21,73 @@ interface PackageMetaDirectory {
   files: (PackageMetaFile | PackageMetaDirectory)[]
 }
 
-function convertMetaToTreeNode(
-  file: PackageMetaFile | PackageMetaDirectory,
-): ITreeNode {
-  switch (file.type) {
-    case 'directory':
-      return {
-        id: file.path,
-        icon: 'folder-close',
-        label: file.path,
-        childNodes: file.files.map(convertMetaToTreeNode),
-        isExpanded: true,
-      }
-    case 'file':
-      return {
-        id: file.path,
-        icon: 'document',
-        label: file.path,
-      }
-  }
-}
-
 const App: React.FC = () => {
-  const [data, setData] = useState<ITreeNode>()
+  const [data, setData] = useState<PackageMetaDirectory>()
+  const [expandedMap, setExpandedMap] = useState<{ [key: string]: boolean }>({})
+  const [code, setCode] = useState('')
 
   useEffect(() => {
     ;(async () => {
       const res = await fetch('https://unpkg.com/express@4.17.1/?meta')
       const json = await res.json()
-      const node = convertMetaToTreeNode(json as PackageMetaDirectory)
-      setData(node)
+      setData(json)
     })()
   }, [])
 
+  if (!data) return null
+
+  const convertMetaToTreeNode = (
+    file: PackageMetaFile | PackageMetaDirectory,
+  ): ITreeNode => {
+    switch (file.type) {
+      case 'directory':
+        return {
+          id: file.path,
+          icon: 'folder-close',
+          label: path.basename(file.path),
+          childNodes: file.files.map(convertMetaToTreeNode),
+          isExpanded: !!expandedMap[file.path],
+        }
+      case 'file':
+        return {
+          id: file.path,
+          icon: 'document',
+          label: path.basename(file.path),
+          secondaryLabel: numeral(file.size).format('0.00b'),
+        }
+    }
+  }
+
+  const files = convertMetaToTreeNode(data).childNodes
+
+  if (!files) return null
+
+  const handleClick = async (node: ITreeNode) => {
+    if (node.icon === 'folder-close') {
+      setExpandedMap(old => ({ ...old, [node.id]: !old[node.id] }))
+    } else {
+      const res = await fetch(`https://unpkg.com/express@4.17.1${node.id}`)
+      const text = await res.text()
+      setCode(text)
+    }
+  }
+
   return (
-    <div>{data && data.childNodes && <Tree contents={data.childNodes} />}</div>
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ flexBasis: 300, overflow: 'auto' }}>
+        <Tree
+          contents={files}
+          onNodeClick={handleClick}
+          onNodeExpand={handleClick}
+          onNodeCollapse={handleClick}
+        />
+      </div>
+      <div style={{ flexGrow: 1, overflow: 'auto' }}>
+        <SyntaxHighlighter language="javascript" style={docco}>
+          {code}
+        </SyntaxHighlighter>
+      </div>
+    </div>
   )
 }
 
