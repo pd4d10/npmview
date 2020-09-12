146f940cea0b8ebc356ad62a3c18211143b9cf8f
Rongjian Zhang <pd4d10@gmail.com>
2020-09-12 23:16:40 +0800

    fix: patch side effect

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './app'
 import 'normalize.css/normalize.css'
-// import '@blueprintjs/core/lib/css/blueprint.css'
+import '@blueprintjs/core/lib/css/blueprint.css'
 import 'github-fork-ribbon-css/gh-fork-ribbon.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
