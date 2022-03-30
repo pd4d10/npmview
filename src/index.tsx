bb4201fbeb03b36064ad74eb4f21da1f0bd184c1
Rongjian Zhang <pd4d10@gmail.com>
2022-03-30 20:54:34 +0800

    chore: update deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { App } from './app'
 import 'normalize.css/normalize.css'
 import '@blueprintjs/core/lib/css/blueprint.css'
 import 'github-fork-ribbon-css/gh-fork-ribbon.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root')!)
+root.render(<App />)
