701d88daa14a91e41de4a940e4caaaf70b463bd8
Rongjian Zhang <pd4d10@gmail.com>
2023-08-20 14:48:41 +0800

    chore: update styles

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 import unocss from "unocss/vite";
 
 export default defineConfig({
-  plugins: [unocss({ include: "src/**/*.js" }), react()],
+  plugins: [unocss({ include: "src/**/*" }), react()],
 });
